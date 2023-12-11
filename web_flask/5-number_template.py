1b3d840bc81c7fd32d154d3676cb54f8b697db82
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-11 12:12:17 -0800

    Fix for checker

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,8 +4,7 @@ Simple Flask web application with routes to display 'Hello HBNB', 'HBNB',
 handle 'C' and 'Python' routes, and display numbers
 """
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -28,11 +27,8 @@ def c_route(text):
 
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
-def python_route(text=None):
-    if text is None:
-        text = 'is cool'
-    else:
-        text = text.replace('_', ' ')
+def python_route(text='is cool'):
+    text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
 
