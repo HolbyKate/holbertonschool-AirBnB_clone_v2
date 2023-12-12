0e0e426ac05e3815b34ade9d3dd14f1a0e92a87d
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-12 07:07:55 -0800

    Fix sorted_states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,8 +19,9 @@ def teardown_appcontext(self):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays HTML page with list of states"""
-    states = storage.all('State')
-    return render_template('7-states_list.html', states=states)
+    states = storage.all('State').values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 if __name__ == '__main__':
