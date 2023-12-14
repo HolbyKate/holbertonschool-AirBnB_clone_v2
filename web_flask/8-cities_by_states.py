5b92eb1e95f1c0abb4af3cfec9e732138b50efff
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-14 13:18:29 -0800

    Fix first line and comment

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
