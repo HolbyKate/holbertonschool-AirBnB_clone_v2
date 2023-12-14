8234d22192d86d629fef41700f2f7a7f0fc06366
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-14 13:01:53 -0800

    Fix LI statement

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -11,10 +12,15 @@ def teardown_appcontext(exception):
     storage.close()
 
 
-@app.route('/cities_by_states', methods=['GET'])
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    states = sorted(storage.all("State").values(), key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=states)
+    """Display a HTML page with a list of all State objects in DBStorage."""
+    states = storage.all(State)
+    cities = storage.all(City)
+
+    return render_template("8-cities_by_states.html",
+                           cities=cities,
+                           states=states)
 
 
 if __name__ == '__main__':
