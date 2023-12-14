3b084882d673dbd4ec9f11178f5a16384db19350
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-14 13:29:20 -0800

    Fix adding code of 7

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""
+Simple Flask web application with routes to display 'Hello HBNB', 'HBNB',
+handle 'C' and 'Python' routes, and display numbers
+"""
 
 from flask import Flask, render_template
 from models import storage
@@ -10,9 +13,56 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_route(text='is cool'):
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown(exception):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_html():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+
+    return render_template("7-states_list.html", states=sorted_states)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
