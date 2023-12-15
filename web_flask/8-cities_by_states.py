48cd77a4e61a068afea580457879a01e1e61af5e
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-15 06:53:02 -0800

    Update for checkers

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,85 +4,95 @@ Simple Flask web application with routes to display 'Hello HBNB', 'HBNB',
 handle 'C' and 'Python' routes, and display numbers
 """
 
-from flask import Flask, redirect, render_template, request
 from models import storage
-from models.state import State
-from models.city import City
+from models import *
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_HBNB():
-    return 'Hello HBNB!'
+@app.route('/')
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def HBNB():
-    return 'HBNB'
+@app.route('/hbnb')
+def hbnb():
+    """display text"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route('/c/<text>')
+def c_text(text):
+    """display custom text given"""
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
-def python_route(text='is cool'):
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """display custom text given
+       first route statement ensures it works for:
+          curl -Ls 0.0.0.0:5000/python ; echo "" | cat -e
+          curl -Ls 0.0.0.0:5000/python/ ; echo "" | cat -e
+    """
+    return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    return '{} is a number'.format(n)
+@app.route('/number/<int:n>')
+def text_if_int(n):
+    """display text only if int given"""
+    return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
+@app.route('/number_template/<int:n>')
+def html_if_int(n):
+    """display html page only if int given
+       place given int into html template
+    """
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', n=n)
+@app.route('/number_odd_or_even/<int:n>')
+def html_odd_or_even(n):
+    """display html page only if int given
+       place given int into html template
+       substitute text to display if int is odd or even
+    """
+    odd_or_even = "even" if (n % 2 == 0) else "odd"
+    return render_template('6-number_odd_or_even.html',
+                           n=n, odd_or_even=odd_or_even)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Remove the current SQLAlchemy Session"""
+def tear_down(self):
+    """after each request remove current SQLAlchemy session"""
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_html():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-
-    return render_template("8-cities_by_states.html", states=sorted_states)
-
-
-@app.route("/add_state", methods=['POST'])
-def add_state():
-    if request.method == 'POST':
-        new_state_name = request.json.get('name')
-
-        new_state = State(name=new_state_name)
-        storage.new(new_state)
-        storage.save()
-
-    return redirect("/states_list")
+@app.route('/states_list')
+def html_fetch_states():
+    """display html page
+       fetch sorted states to insert into html in UL tag
+    """
+    state_objs = [s for s in storage.all("State").values()]
+    return render_template('7-states_list.html',
+                           state_objs=state_objs)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
+@app.route('/cities_by_states')
+def html_fetch_cities_by_states():
+    """display html page
+       fetch sorted states to insert into html in UL tag
+       fetch sorted cities in each state into LI tag ->in HTML file
+    """
+    state_objs = [s for s in storage.all("State").values()]
+    return render_template('8-cities_by_states.html',
+                           state_objs=state_objs)
 
-    return render_template("8-cities_by_states.html", states=sorted_states)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
